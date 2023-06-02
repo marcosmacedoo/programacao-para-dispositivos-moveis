0790a7b3693f5efc67911263136e6553298fd502
Marcos Macedo <marcosmacedoo.dev@gmail.com>
2023-06-02 17:25:10 -0300

    refactor: change positions and border radius elevated button

--- app_contacts/lib/pages/login.dart
+++ app_contacts/lib/pages/login.dart
@@ -7,13 +7,13 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Projeto Final',
+      title: 'App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
       home: const LoginPage(),
-      debugShowMaterialGrid: false,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -35,36 +35,46 @@ class _LoginPage extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [
-            Image(
-              width: 150,
-              image: NetworkImage(
-                  'https://pbs.twimg.com/profile_images/438771627854024704/Az4OY07a_400x400.png'),
-            ),
-            TextField(
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (value) => username = value,
-                decoration: const InputDecoration(label: Text("Email"))),
-            Padding(
-              padding: const EdgeInsets.only(top: 16.0),
-              child: TextField(
-                  keyboardType: TextInputType.visiblePassword,
-                  onChanged: (value) => password = value,
-                  decoration: const InputDecoration(label: Text("Senha"))),
-            ),
-            Padding(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(
+                width: 150,
+                image: NetworkImage(
+                    'https://pbs.twimg.com/profile_images/438771627854024704/Az4OY07a_400x400.png'),
+              ),
+              Padding(
                 padding: const EdgeInsets.only(top: 32.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 48,
-                  child: ElevatedButton(
-                      onPressed: _singIn,
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.yellow),
-                      child: Text("Entrar")),
-                ))
-          ],
+                child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    onChanged: (value) => username = value,
+                    decoration: const InputDecoration(label: Text("Email"))),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: TextField(
+                    keyboardType: TextInputType.visiblePassword,
+                    onChanged: (value) => password = value,
+                    decoration: const InputDecoration(label: Text("Senha"))),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 32.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 48,
+                    child: ElevatedButton(
+                        onPressed: _singIn,
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5)),
+                          backgroundColor: Colors.yellow[600],
+                        ),
+                        child: const Text("Entrar")),
+                  ))
+            ],
+          ),
         ),
       ),
     );
